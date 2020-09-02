d49aa51eb13991d3f1c1a5e0d586c49232c4846d
Yan Qian <qianyan@thoughtworks.com>
2020-09-02 23:14:27 +0800

    fix typo of kubctl short name

--- test-network-k8s/01-ORDERER/sync.sh
+++ test-network-k8s/01-ORDERER/sync.sh
@@ -3,23 +3,22 @@
 : ${NS:=testnet}
 
 K="kubectl -n $NS"
-source_path=
 app=orderer
-domain=$NS.svc.cluster.local
-source_path=../crypto-config/ordererOrganizations/$domain/orderers/$app.$domain
+domain="$NS.svc.cluster.local"
+source_path=../../first-network/crypto-config/ordererOrganizations/$domain/orderers/$app.$domain
 target_path=/var/hyperledger/orderer
 
 set -x
-local POD=$($K get pods -l app=orderer -o=jsonpath='{.items[0].metadata.name}')
+POD=$($K get pods -l app=orderer -o=jsonpath='{.items[0].metadata.name}')
 set +x
 
 echo "Sync crypto config to $POD"
 
 set -x
-$K cp $source_path/msp $NS/$POD:$target_path/msp
-$k exec $POD -- ls -l $target_path/msp
+$K cp $source_path/msp $NS/$POD:$target_path/
+$K exec $POD -- ls -l $target_path/msp
 
-$K cp $source_path/tls $NS/$POD:$target_path/tls
-$k exec $POD -- ls -l $target_path/tls
+$K cp $source_path/tls $NS/$POD:$target_path/
+$K exec $POD -- ls -l $target_path/tls
 set +x
 
